9320eec32f3d1b5ef9499c399196524a86f3ce6e
Jaime Lopez <jaime.a.lopez@gmail.com>
2023-05-04 16:18:49 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+// Desc: This is the main server file for the application
+
+const express = require('express');
+const polygonController = require('./controllers/polygonController');
+const app = express();
+require('dotenv').config();
+const chalk = require('chalk');
+const routes = require('./routes/routes');
+const errorHandler = require('./middleware/errorHandler');
+const logger = require('./middleware/logger');
+
+port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Middleware to parse JSON data from request body
+app.use(express.json());
+
+// logger middleware
+app.use(logger);
+
+// import routes
+app.use(routes);
+
+// Middleware to handle errors
+app.use(errorHandler);
+
+app.listen(port, () => {
+  // use chalk to color the console log and make it stand out
+  console.log(chalk.blue.bold(`Stock app listening on port ${port}!`));
+});
+
+
+module.exports = app;
